ad0869367b5d213afc89b9f0a5e1524dc39c4fcd
lsq@ubt <1076852883@qq.com>
2019-11-25 15:46:07 +0800

    make STAR2THU compatible
    Summary: make STAR2THU compatible
    
    Test Plan: NA
    
    Reviewers: hmx, ruanhuabin
    
    Reviewed By: hmx, ruanhuabin
    
    Subscribers: wangxiaoya
    
    Differential Revision: http://124.65.131.150:11111/D286

--- script/STAR_2_THU.py
+++ script/STAR_2_THU.py
@@ -3,7 +3,7 @@
 # author Mingxu Hu
 # author Hongkun Yu
 # author Shouqing Li
-# 
+#
 # version 1.4.11.081101
 # copyright THUNDER Non-Commercial Software License Agreement
 #
@@ -39,7 +39,7 @@ def main():
     prog_name = os.path.basename(sys.argv[0])
     usage = """
     Transform STAR file to THU file.
-    {prog} < -i input_star file> < -o output_thu file > 
+    {prog} < -i input_star file> < -o output_thu file >
     """.format(prog = prog_name)
 
     optParser = OptionParser(usage)
@@ -58,10 +58,11 @@ def main():
     (options, args) = optParser.parse_args()
 
     header_dict = {}
+    flag = 0
 
     if len(sys.argv) == 1:
-      print usage
-      print "    For more detail, see '-h' or '--help'."
+        print usage
+        print "    For more detail, see '-h' or '--help'."
 
     if options.output_thu:
         fout = open(options.output_thu, "w")
@@ -72,121 +73,304 @@ def main():
             print "Please input a proper thu file."
             exit()
 
-        for num, line in enumerate(fin):
+        for line in fin.readlines():
 
             if (line[0] == '#'):
+                flag = flag + 1
+                continue
+            else:
                 continue
 
-            sline = line.strip()
-            if not sline or (' ' not in sline):
-                continue
+        fin.close()
+        fin = open(options.input_star, "r")
 
-            match = re.match(r'_rln(\w+)\s+#(\d+)', sline)
-            if match:
-                header_dict[match.group(1).lower()] = int(match.group(2)) - 1
-                continue
-            sp = sline.split()
+        if flag == 1 or flag == 0:
+            for num, line in enumerate(fin):
 
-            try:
-                ps = float(sp[header_dict['phaseshift']]) * (math.pi / 180)
-            except (ValueError, IndexError):
-                sys.stderr.write(
-                    'Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
-                continue
-            except KeyError:
-                ps = 0
-                #sys.stderr.write('Warning: line #{} ({}) missed phaseshift\n'.format(num + 1, sline))
+                if (line[0] == '#'):
+                    continue
 
-            try:
-                gI = int(sp[header_dict['groupnumber']])
-            except (ValueError, IndexError):
-                sys.stderr.write(
-                    'Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
-                continue
-            except KeyError:
-                gI = 1
-                #sys.stderr.write('Warning: line #{} ({}) missed groupnumber\n'.format(num + 1, sline))
+                sline = line.strip()
 
-            try:
-                volt = float(sp[header_dict['voltage']]) * 1000.0
-                dU = float(sp[header_dict['defocusu']])
-                dV = float(sp[header_dict['defocusv']])
-                dT = float(sp[header_dict['defocusangle']]) * (math.pi / 180)
-                c = float(sp[header_dict['sphericalaberration']]) * 1e7
-                aC = float(sp[header_dict['amplitudecontrast']])
-                iN = sp[header_dict['imagename']]
-                mN = sp[header_dict['micrographname']]
-                coordX = float(sp[header_dict['coordinatex']])
-                coordY = float(sp[header_dict['coordinatey']])
+                if not sline or (' ' not in sline):
+                    continue
 
-                phi = float(sp[header_dict['anglerot']]) 
-                theta = float(sp[header_dict['angletilt']]) 
-                psi = float(sp[header_dict['anglepsi']]) 
+                match = re.match(r'_rln(\w+)\s+#(\d+)', sline)
 
-                quat0, quat1, quat2, quat3 = euler_to_quaternion([phi, theta, psi])
+                if match:
+                    header_dict[match.group(1).lower()] = int(match.group(2)) - 1
+                    continue
 
-            except (ValueError, IndexError):
-                sys.stderr.write(
-                    'Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
-                continue
-            except KeyError as e:
-                sys.stderr.write(
-                    'Header does not include {}. This is an invalid star file\n'.format(str(e)))
-                sys.exit(2)
+                sp = sline.split()
 
-            fout.write('{volt:18.6f} \
-                        {dU:18.6f} \
-                        {dV:18.6f} \
-                        {dT:18.6f} \
-                        {c:18.6f} \
-                        {aC:18.6f} \
-                        {phaseShift:18.6f} \
-                        {iN} \
-                        {mN} \
-                        {coordX:18.6f} \
-                        {coordY:18.6f} \
-                        {gI} \
-                        {classI} \
-                        {quat0:18.6f} \
-                        {quat1:18.6f} \
-                        {quat2:18.6f} \
-                        {quat3:18.6f} \
-                        {stdRot0:18.6f} \
-                        {stdRot1:18.6f} \
-                        {stdRot2:18.6f} \
-                        {transX:18.6f} \
-                        {transY:18.6f} \
-                        {stdTransX:18.6f} \
-                        {stdTransY:18.6f} \
-                        {df:18.6f} \
-                        {stdDf:18.6f} \
-                        {score:18.6f} \n'.format(volt = volt,
-                                          dU = dU,
-                                          dV = dV,
-                                          dT = dT,
-                                          c = c,
-                                          aC = aC,
-                                          phaseShift = ps,
-                                          iN = iN,
-                                          mN = mN,
-                                          coordX = coordX,
-                                          coordY = coordY,
-                                          gI = gI,
-                                          classI = 0,
-                                          quat0 = quat0,
-                                          quat1 = quat1,
-                                          quat2 = quat2,
-                                          quat3 = quat3,
-                                          stdRot0 = 0,
-                                          stdRot1 = 0,
-                                          stdRot2 = 0,
-                                          transX = 0,
-                                          transY = 0,
-                                          stdTransX = 0,
-                                          stdTransY = 0,
-                                          df = 1,
-                                          stdDf = 0,
-                                          score = 0))
+                try:
+                    ps = float(sp[header_dict['phaseshift']]) * (math.pi / 180)
+
+                except (ValueError, IndexError):
+                    sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                    continue
+
+                except KeyError:
+                    ps = 0
+
+                try:
+                    gI = int(sp[header_dict['groupnumber']])
+
+                except (ValueError, IndexError):
+                    sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                    continue
+
+                except KeyError:
+                    gI = 1
+
+                try:
+                    volt = float(sp[header_dict['voltage']]) * 1000.0
+                    dU = float(sp[header_dict['defocusu']])
+                    dV = float(sp[header_dict['defocusv']])
+                    dT = float(sp[header_dict['defocusangle']]) * (math.pi / 180)
+                    c = float(sp[header_dict['sphericalaberration']]) * 1e7
+                    aC = float(sp[header_dict['amplitudecontrast']])
+                    iN = sp[header_dict['imagename']]
+                    mN = sp[header_dict['micrographname']]
+                    coordX = float(sp[header_dict['coordinatex']])
+                    coordY = float(sp[header_dict['coordinatey']])
+
+                except (ValueError, IndexError):
+                    sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                    continue
+
+                except KeyError as e:
+                    sys.stderr.write('Header does not include {}. This is an invalid star file\n'.format(str(e)))
+                    sys.exit(2)
+
+                try:
+                    phi = float(sp[header_dict['anglerot']])
+                    theta = float(sp[header_dict['angletilt']])
+                    psi = float(sp[header_dict['anglepsi']])
+
+                except (ValueError, IndexError):
+                    sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                    continue
+
+                except KeyError:
+                    phi = 0
+                    theta = 0
+                    psi = 0
+
+                    quat0, quat1, quat2, quat3 = euler_to_quaternion([phi, theta, psi])
+
+                    fout.write('{volt:18.6f} \
+                                {dU:18.6f} \
+                                {dV:18.6f} \
+                                {dT:18.6f} \
+                                {c:18.6f} \
+                                {aC:18.6f} \
+                                {phaseShift:18.6f} \
+                                {iN} \
+                                {mN} \
+                                {coordX:18.6f} \
+                                {coordY:18.6f} \
+                                {gI} \
+                                {classI} \
+                                {quat0:18.6f} \
+                                {quat1:18.6f} \
+                                {quat2:18.6f} \
+                                {quat3:18.6f} \
+                                {stdRot0:18.6f} \
+                                {stdRot1:18.6f} \
+                                {stdRot2:18.6f} \
+                                {transX:18.6f} \
+                                {transY:18.6f} \
+                                {stdTransX:18.6f} \
+                                {stdTransY:18.6f} \
+                                {df:18.6f} \
+                                {stdDf:18.6f} \
+                                {score:18.6f} \n'.format(volt = volt,
+                                                  dU = dU,
+                                                  dV = dV,
+                                                  dT = dT,
+                                                  c = c,
+                                                  aC = aC,
+                                                  phaseShift = ps,
+                                                  iN = iN,
+                                                  mN = mN,
+                                                  coordX = coordX,
+                                                  coordY = coordY,
+                                                  gI = gI,
+                                                  classI = 0,
+                                                  quat0 = quat0,
+                                                  quat1 = quat1,
+                                                  quat2 = quat2,
+                                                  quat3 = quat3,
+                                                  stdRot0 = 0,
+                                                  stdRot1 = 0,
+                                                  stdRot2 = 0,
+                                                  transX = 0,
+                                                  transY = 0,
+                                                  stdTransX = 0,
+                                                  stdTransY = 0,
+                                                  df = 1,
+                                                  stdDf = 0,
+                                                  score = 0))
+
+        if flag == 2:
+
+            part = 0
+            door = 1
+            dic = []
+
+            for num, line in enumerate(fin):
+                if (line[0] == '#'):
+                    part = part + 1
+                    continue
+
+                if (door == 1) and (part == 2):
+                    optics_dict = header_dict
+                    header_dict = {}
+                    door = 0
+
+                sline = line.strip()
+
+                if not sline or (' ' not in sline):
+                    continue
+
+                match = re.match(r'_rln(\w+)\s+#(\d+)', sline)
+
+                if match:
+                    header_dict[match.group(1).lower()] = int(match.group(2)) - 1
+                    continue
+
+                sp = sline.split()
+
+                if part == 1:
+
+                    dic.append(sp)
+
+                if part == 2:
+
+                    try:
+                        gp = int(sp[header_dict['opticsgroup']])
+
+                    except (ValueError, IndexError):
+                        sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                        continue
+
+                    except KeyError as e:
+                        sys.stderr.write('Header does not include {}. This is an invalid star file\n'.format(str(e)))
+                        sys.exit(2)
+
+                    else:
+                        volt = float(dic[gp - 1][optics_dict['voltage']]) * 1000.0
+                        c = float(dic[gp - 1][optics_dict['sphericalaberration']]) * 1e7
+                        aC = float(dic[gp - 1][optics_dict['amplitudecontrast']])
+
+                    try:
+                        ps = float(sp[header_dict['phaseshift']]) * (math.pi / 180)
+
+                    except (ValueError, IndexError):
+                        sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                        continue
+
+                    except KeyError:
+                        ps = 0
+
+                    try:
+                        gI = int(sp[header_dict['groupnumber']])
+
+                    except (ValueError, IndexError):
+                        sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                        continue
+
+                    except KeyError:
+                        gI = 1
+
+                    try:
+                        phi = float(sp[header_dict['anglerot']])
+                        theta = float(sp[header_dict['angletilt']])
+                        psi = float(sp[header_dict['anglepsi']])
+
+                    except (ValueError, IndexError):
+                        sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                        continue
+
+                    except KeyError:
+                        phi = 0
+                        theta = 0
+                        psi = 0
+
+                    try:
+                        dU = float(sp[header_dict['defocusu']])
+                        dV = float(sp[header_dict['defocusv']])
+                        dT = float(sp[header_dict['defocusangle']]) * (math.pi / 180)
+                        iN = sp[header_dict['imagename']]
+                        mN = sp[header_dict['micrographname']]
+                        coordX = float(sp[header_dict['coordinatex']])
+                        coordY = float(sp[header_dict['coordinatey']])
+
+                    except (ValueError, IndexError):
+                        sys.stderr.write('Warning: skipping line #{} ({}) that cannot be parsed\n'.format(num + 1, sline))
+                        continue
+
+                    except KeyError as e:
+                        sys.stderr.write('Header does not include {}. This is an invalid star file\n'.format(str(e)))
+                        sys.exit(2)
+
+                    quat0, quat1, quat2, quat3 = euler_to_quaternion([phi, theta, psi])
+
+                    fout.write('{volt:18.6f} \
+                                {dU:18.6f} \
+                                {dV:18.6f} \
+                                {dT:18.6f} \
+                                {c:18.6f} \
+                                {aC:18.6f} \
+                                {phaseShift:18.6f} \
+                                {iN} \
+                                {mN} \
+                                {coordX:18.6f} \
+                                {coordY:18.6f} \
+                                {gI} \
+                                {classI} \
+                                {quat0:18.6f} \
+                                {quat1:18.6f} \
+                                {quat2:18.6f} \
+                                {quat3:18.6f} \
+                                {stdRot0:18.6f} \
+                                {stdRot1:18.6f} \
+                                {stdRot2:18.6f} \
+                                {transX:18.6f} \
+                                {transY:18.6f} \
+                                {stdTransX:18.6f} \
+                                {stdTransY:18.6f} \
+                                {df:18.6f} \
+                                {stdDf:18.6f} \
+                                {score:18.6f} \n'.format(volt = volt,
+                                                  dU = dU,
+                                                  dV = dV,
+                                                  dT = dT,
+                                                  c = c,
+                                                  aC = aC,
+                                                  phaseShift = ps,
+                                                  iN = iN,
+                                                  mN = mN,
+                                                  coordX = coordX,
+                                                  coordY = coordY,
+                                                  gI = gI,
+                                                  classI = 0,
+                                                  quat0 = quat0,
+                                                  quat1 = quat1,
+                                                  quat2 = quat2,
+                                                  quat3 = quat3,
+                                                  stdRot0 = 0,
+                                                  stdRot1 = 0,
+                                                  stdRot2 = 0,
+                                                  transX = 0,
+                                                  transY = 0,
+                                                  stdTransX = 0,
+                                                  stdTransY = 0,
+                                                  df = 1,
+                                                  stdDf = 0,
+                                                  score = 0))
+
         fin.close()
         fout.close()
 
